0d23fbe43ec0b32e1c8de72659b231d728f22120
Elena Vinogradova <elena20031999@gmail.com>
2023-06-15 12:32:21 +0200

    add junit4

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -3,31 +3,31 @@ import ru.netology.CashbackHackService;
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void cashbackBelowBoundary() {
         int actual = service.remain(900);
         int expected = 100;
-        org.testng.AssertJUnit.assertEquals(expected, actual);
+        org.junit.Assert.assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void cashbackBoundary() {
         int actual = service.remain(1000);
         int expected = 0;
-        org.testng.AssertJUnit.assertEquals(expected, actual);
+        org.junit.Assert.assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void cashbackAboveBoundary() {
         int actual = service.remain(1100);
         int expected = 900;
-        org.testng.AssertJUnit.assertEquals(expected, actual);
+        org.junit.Assert.assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void cashbackZeroAmount() {
         int actual = service.remain(0);
         int expected = 1000;
-        org.testng.AssertJUnit.assertEquals(expected, actual);
+        org.junit.Assert.assertEquals(expected, actual);
     }
 }
